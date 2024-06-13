1ee1001da9727a2e37cf901ff36f5c71c295a82d
Lyne Som <l7204348@gmail.com>
2024-06-13 14:17:23 +0100

    declaration des attributs de la classe property_title

--- properties/models.py
+++ properties/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Property_type(models.Model):
+    land_title = models.FileField(upload_to=)
+    types = models.CharField(max_length=200)
+    city = models.CharField(max_length=100)
+    neighborood = models.CharField(max_length=100)
+    standing = models.CharField(max_length=100)
+    status = models.CharField(max_length=100)
+    price = models.BigIntegerField()
+    description = models.TextField()
+    
