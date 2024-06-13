1703a28341e218c44dfb6980b3a9e0a01120bcd9
Lyne Som <l7204348@gmail.com>
2024-06-13 14:34:59 +0100

    class property_type save

--- properties/models.py
+++ properties/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Property_type(models.Model):
-    land_title = models.FileField(upload_to=)
+    land_title = models.FileField(upload_to='/land_titles/')
     types = models.CharField(max_length=200)
     city = models.CharField(max_length=100)
     neighborood = models.CharField(max_length=100)
