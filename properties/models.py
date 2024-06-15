38ee470bc35808792e0172cae3daa2886b49cc7d
Tiam-Rietsch <NgnotchoueTiam@gmail.com>
2024-06-15 16:57:17 +0100

    proprietor and tenant foreign keys

--- properties/models.py
+++ properties/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Property(models.Model):
+    proprietor = models.ForeignKey('ProprietorProfile', on_delete=models.CASCADE, related_name='properties')
+    tenant = models.ForeignKey('BuyerProfile', on_delete=models.SET_NULL, related_name='properties')
     land_title = models.FileField(upload_to='land_titles/')
     property_type = models.CharField(max_length=200)
     city = models.CharField(max_length=100)
