86d7cce165627210e1cc91ab5fb5e0c2efd13f77
Tiam-Rietsch <NgnotchoueTiam@gmail.com>
2024-06-15 17:55:40 +0100

    resolved property model

--- properties/models.py
+++ properties/models.py
@@ -6,8 +6,8 @@ from users.models import (
 
 # Create your models here.
 class Property(models.Model):
-    proprietor = models.ForeignKey(ProprietorProfile, on_delete=models.CASCADE, related_name='properties')
-    tenant = models.ForeignKey(BuyerProfile, on_delete=models.SET_NULL, related_name='properties')
+    proprietor = models.ForeignKey(ProprietorProfile, on_delete=models.CASCADE, related_name='properties', blank=True, null=True)
+    tenant = models.ForeignKey(BuyerProfile, on_delete=models.SET_NULL, related_name='properties', blank=True, null=True)
     land_title = models.FileField(upload_to='land_titles/')
     property_type = models.CharField(max_length=200)
     city = models.CharField(max_length=100)
