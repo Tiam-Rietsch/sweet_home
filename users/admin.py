a28073679d3559b34a3f60c814ac327fdbc0f76a
perigmessanga <messangaperig3@gmail.com>
2024-06-15 16:44:00 +0100

    prorietorprofile

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-
+from .models import ProprietorProfile
+from .models import BuyerProfile
 from .models import User
 
 admin.site.register(User)
+admin.site.register(ProprietorProfile)
+admin.site.register(BuyerProfile)
--- users/models.py
+++ users/models.py
@@ -12,3 +12,15 @@ class User(AbstractUser):
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["first_name", "last_name", "username"]
+
+
+class ProprietorProfile(models.Model):
+    user = models.OneToOneField("User", on_delete= models.CASCADE, related_name= "proprietor_profile", blank=True, null=True )
+    cni_recto = models.ImageField(blank=True, null=True, upload_to="profile_picture/")
+    cni_verso = models.ImageField(blank=True, null=True, upload_to="profile_picture/")
+    facture_recto = models.ImageField(blank=True, null=True, upload_to="profile_picture/")
+    facture_verso = models.ImageField(blank=True, null=True, upload_to="profile_picture/")
+    status = models.CharField(max_length=50, blank=True, null=True)
+
+class BuyerProfile(models.Model):
+    user = models.OneToOneField("User", on_delete= models.CASCADE, related_name="buyerprofile", blank=True, null=True)
