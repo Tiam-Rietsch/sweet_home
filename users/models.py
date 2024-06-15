80cdaab7db439261fc406e528988ceaad3410073
Tiam-Rietsch <NgnotchoueTiam@gmail.com>
2024-06-15 15:34:45 +0100

    user model working

--- users/models.py
+++ users/models.py
@@ -4,11 +4,11 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class User(AbstractUser):
     email = models.EmailField(unique=True)
-    phone = models.BigIntegerField()
-    city = models.CharField(max_length=200)
-    address = models.CharField(max_length=200)
-    neighbor_hood = models.CharField(max_length=200)
-    profile_picture = models.ImageField(upload_to='profile_picture/')
+    phone = models.BigIntegerField(blank=True, null=True)
+    city = models.CharField(max_length=200, blank=True, null=True)
+    address = models.CharField(max_length=200, blank=True, null=True)
+    neighbor_hood = models.CharField(max_length=200, blank=True, null=True)
+    profile_picture = models.ImageField(upload_to='profile_picture/', blank=True, null=True)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["first_name", "last_name", "username"]
