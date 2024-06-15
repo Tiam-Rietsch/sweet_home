910165c72a927de893e6b629331e874e71222f5b
perigmessanga <messangaperig3@gmail.com>
2024-06-15 14:56:11 +0100

    user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    phone = models.BigIntegerField()
+    city = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
+    neighbor_hood = models.CharField(max_length=200)
+    profile_picture = models.ImageField(upload_to='profile_picture/')
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["first_name", "last_name", "username"]
