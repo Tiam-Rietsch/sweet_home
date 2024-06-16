f33d4d2786f1560690610c54e991ac12b6fd5c59
perigmessanga <messangaperig3@gmail.com>
2024-06-16 14:04:16 +0100

    creation forms.py

---
+++ users/forms.py
@@ -0,0 +1,12 @@
+from .models import ProprietorProfile
+from django.forms.models import ModelForm 
+
+class ProprietorPofileForm(ModelForm):
+    
+    
+    
+       
+
+    class Meta:
+        model=ProprietorProfile
+        fields=["cni_recto", "cni_verso", "facture_recto", "facture_verso","status"]
